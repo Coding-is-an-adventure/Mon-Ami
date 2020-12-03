65c90612af47f14af69051aa181d5fbda9bc45e1
Tony <tony.zhou125@gmail.com>
2020-12-03 20:39:51 +0100

    Cleaned up the FE code
    Added Semantic UI CSS to fix the style.
    Removed the logging for the API.

--- mon-ami.client/src/App.tsx
+++ mon-ami.client/src/App.tsx
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-
-import './App.css';
+import React, { Component } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
-
   state = {
-    values: []
-  }
+    values: [],
+  };
 
-  componentDidMount () {
-    axios.get('https://localhost:44380/api/Values')
-    .then((response) => {
-      console.log("Fetched data!")
-      console.log(response.data)
+  componentDidMount() {
+    axios.get("https://localhost:44380/api/Values").then((response) => {
       this.setState({
-        values: response.data
-      })
-    })
+        values: response.data,
+      });
+    });
   }
 
   render() {
     return (
-      <div className='App'>
-        <ul>
-          {this.state.values.map((value: any) =>
-          <li key={value.id}>{value.name}</li>
-          )}
-        </ul>
+      <div className="App">
+        <Header as="h2">
+          <Icon name="users"></Icon>
+            <Header.Content>Mon Ami</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
       </div>
     );
   }
