bd9b1f4e055381425ff65fc47f3489fb4150de13
Tony <tony.zhou125@gmail.com>
2020-12-03 17:45:27 +0100

    Fix Cors Error

--- mon-ami.client/src/App.tsx
+++ mon-ami.client/src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import { render } from "@testing-library/react";
+import Axios, { AxiosResponse } from "axios";
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: [],
+  };
+
+  componentDidMount() {
+    axios.get('https://localhost:44380/api/values')
+    .then((response: AxiosResponse<any>) => {
+      console.log(response);
+      this.setState({
+        values: response.data
+      })
+    })   
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.values.map((value: any) => {
+            <li key={value.id}>{value.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
