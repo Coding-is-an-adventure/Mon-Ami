df3645363426b3f1afd69fce6bde693d5ca7c199
Tony <tony.zhou125@gmail.com>
2020-12-03 20:14:09 +0100

    Fixed Typo in the Cors Policy
    Updated startup.cs

--- Mon-Ami.Server/Mon-Ami.API/Startup.cs
+++ Mon-Ami.Server/Mon-Ami.API/Startup.cs
@@ -37,7 +37,7 @@ namespace Mon_Ami.API
             {
                 options.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000/");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
             services.AddControllers();
