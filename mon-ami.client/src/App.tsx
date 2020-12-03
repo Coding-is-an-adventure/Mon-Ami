f98dfa1a793e0d29987d663769c2cdc3d6c01567
Tony <tony.zhou125@gmail.com>
2020-12-03 20:17:41 +0100

    Added Fetch Test

--- mon-ami.client/src/App.tsx
+++ mon-ami.client/src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    values: []
+  }
+
+  componentDidMount () {
+    axios.get('https://localhost:44380/api/Values')
+    .then((response) => {
+      console.log("Fetched data!")
+      console.log(response.data)
+      this.setState({
+        values: response.data
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hi</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
