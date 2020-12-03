d70603876eadf22f5ecb6ba386d54f65186b639a
Tony <tony.zhou125@gmail.com>
2020-12-03 14:16:30 +0100

    Added DbContext and DbSet

---
+++ Mon-Ami.Server/API.Persistence/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
