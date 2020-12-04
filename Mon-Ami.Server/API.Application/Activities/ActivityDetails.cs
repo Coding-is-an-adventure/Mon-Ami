c32859cc679f379e5ce756c8a6de6342338237d6
Tony <tony.zhou125@gmail.com>
2020-12-04 01:27:18 +0100

    Create ActivityDetails.cs

---
+++ Mon-Ami.Server/API.Application/Activities/ActivityDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using API.Domain;
+using API.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Application.Activities
+{
+    public class ActivityDetails
+    {
+        public class Query : IRequest<Activity>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Activity>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Activity> Handle(
+                Query request,
+                CancellationToken cancellationToken)
+            {
+                Activity activitie = await _context.Activities.FindAsync(request.Id);
+                return activitie;
+            }
+        }
+    }
+}
