affb7581d772aa43347c51ad1cd6935cc5879ef0
Tony <tony.zhou125@gmail.com>
2020-12-04 02:49:10 +0100

    Create ActivityDelete.cs

---
+++ Mon-Ami.Server/API.Application/Activities/ActivityDelete.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using API.Persistence;
+using MediatR;
+
+namespace API.Application.Activities
+{
+    public class ActivityDelete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(
+                Command request,
+                CancellationToken token)
+            {
+
+                _context.Activities.Remove();
+                bool succes = await _context.SaveChangesAsync(token) > 0;
+                if (succes == true)
+                {
+                    return Unit.Value;
+                }
+
+                // Throws an error if the count is equal or smaller than 0.
+                // This means that 0 changes have been made in the database.
+                throw new Exception("A problem occured while trying to save the changes.");
+            }
+        }
+}
