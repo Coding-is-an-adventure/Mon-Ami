40098c60040e5086af8afdfc3a1158369f836393
Tony <tony.zhou125@gmail.com>
2020-12-04 02:13:22 +0100

    Reformat Names

--- Mon-Ami.Server/Mon-Ami.API/Controllers/ActivitiesController.cs
+++ Mon-Ami.Server/Mon-Ami.API/Controllers/ActivitiesController.cs
@@ -23,16 +23,23 @@ namespace Mon_Ami.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> ActivityList(CancellationToken token)
+        public async Task<ActionResult<List<Activity>>> GetActivityList(CancellationToken token)
         {
             List<Activity> result = await _mediator.Send(new ActivityList.Query(), token);
             return result;
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> ActivityDetails(Guid id, CancellationToken token)
+        public async Task<ActionResult<Activity>> GetActivity(Guid id, CancellationToken token)
         {
             Activity result = await _mediator.Send(new ActivityDetails.Query { Id = id }, token);
+            return result;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> CreateActivity(ActivityCreate.Command command)
+        {
+            Unit result = await _mediator.Send(command);
             return result;
         }
     }
