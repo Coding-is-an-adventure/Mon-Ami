9370a4f7d5fc0f9590ffa8170660a9c1968dcf0a
Tony <tony.zhou125@gmail.com>
2020-12-04 02:28:22 +0100

    Create ActivityEdit.cs

---
+++ Mon-Ami.Server/API.Application/Activities/ActivityEdit.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using API.Domain;
+using API.Persistence;
+using MediatR;
+
+namespace API.Application.Activities
+{
+    public class ActivityEdit
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+
+            public string Title { get; set; }
+
+            public string Description { get; set; }
+
+            public string Category { get; set; }
+
+            public DateTime? Date { get; set; }
+
+            public string City { get; set; }
+
+            public string Venue { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(
+                Command request,
+                CancellationToken token)
+            {
+                Activity activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                {
+                    throw new Exception("Could not find the specified activity");
+                }
+
+                // if the left side is null, everything on the right of the operator will be executed.
+                activity.Title = request.Title ?? activity.Title;
+                activity.Description = request.Description ?? activity.Description;
+                activity.Category = request.Category ?? activity.Category;
+                activity.Date = request.Date ?? activity.Date;
+                activity.City = request.City ?? activity.City;
+                activity.Venue = request.Venue ?? activity.Venue;
+
+                bool succes = await _context.SaveChangesAsync() > 0;
+                if (succes == true)
+                {
+                    return Unit.Value;
+                }
+
+                // Throws an error if the count is equal or smaller than 0.
+                // This means that 0 changes have been made in the database.
+                throw new Exception("A problem occured while trying to save the changes.");
+            }
+        }
+    }
+}
