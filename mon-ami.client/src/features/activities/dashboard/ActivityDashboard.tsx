eca48ad7d30d93f8aa0ab2ca6be78a8610968c18
Tony <tony.zhou125@gmail.com>
2020-12-08 01:28:06 +0100

    Update ActivityDashboard.tsx

--- mon-ami.client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ mon-ami.client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,7 @@ import { Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/Activity";
 import ActivityList from "../dashboard/ActivityList";
 import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface IActivityDashboardProps {
   activities: IActivity[];
@@ -13,11 +14,12 @@ const ActivityDashboard: React.FC<IActivityDashboardProps> = ({
 }) => {
   return (
     <Grid>
-      <Grid.Column width={10}>
-        <ActivityList activities={activities} />
-      </Grid.Column>
       <Grid.Column width={6}>
         <ActivityDetails />
+        <ActivityForm />
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <ActivityList activities={activities} />
       </Grid.Column>
     </Grid>
   );
