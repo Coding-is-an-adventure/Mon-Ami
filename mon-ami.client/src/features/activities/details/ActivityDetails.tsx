425d1be2ff94aa3c16a7ee042cfa4938754de003
Tony <tony.zhou125@gmail.com>
2020-12-08 01:08:59 +0100

    Update ActivityDetails.tsx

--- mon-ami.client/src/features/activities/details/ActivityDetails.tsx
+++ mon-ami.client/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Card, Image, Button } from "semantic-ui-react";
 
-interface ActivityDetailsProps {
+interface ActivityDetailsProps {}
 
-}
+const ActivityDetails: React.FC<ActivityDetailsProps> = () => {
+  return (
+    <Card fluid>
+      <Image src="/assets/placeholder.png" wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>Title</Card.Header>
+        <Card.Meta>
+          <span>Date</span>
+        </Card.Meta>
+        <Card.Description>
+          <span>Description</span>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group widths={2}>
+          <Button basic color="blue" content='Edit'/>
+          <Button basic color="grey" content='Cancel'/>
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
+};
 
-export const ActivityDetails: React.FC<ActivityDetailsProps> = () => {
-        return (<div></div>);
-}
+export default ActivityDetails;
