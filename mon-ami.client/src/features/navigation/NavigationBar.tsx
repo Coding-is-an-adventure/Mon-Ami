f71c5c031ce0df9e31ffb4e48f97e43e9f00df28
Tony <tony.zhou125@gmail.com>
2020-12-08 01:27:57 +0100

    Update NavigationBar.tsx

--- mon-ami.client/src/features/navigation/NavigationBar.tsx
+++ mon-ami.client/src/features/navigation/NavigationBar.tsx
@@ -7,13 +7,17 @@ const NavigationBar: React.FC<NavigationBarProps> = () => {
   return (
     <Menu fixed="top" inverted>
       <Container>
-        <Menu.Item header> 
-            <img src="/assets/logo.png" alt="logo" style={{marginRight:"10px"}}/>
-            Mon Ami
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          Mon Ami
         </Menu.Item>
         <Menu.Item name="Activities" />
         <Menu.Item name="Create Activity">
-            <Button positive content="Create Activity"/>
+          <Button positive content="Create Activity" />
         </Menu.Item>
       </Container>
     </Menu>
