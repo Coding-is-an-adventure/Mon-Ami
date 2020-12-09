6d65942ca15da314f60a34d87e2058c1aa250fd4
Tony <tony.zhou125@gmail.com>
2020-12-09 03:23:56 +0100

    Create Loading.tsx
    Added a loading circle to show user feedback.

---
+++ mon-ami.client/src/app/layout/Loading.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Dimmer, Loader } from 'semantic-ui-react';
+
+interface LoadingProps {
+    inverted?: boolean;
+    content?: string;
+}
+
+const Loading: React.FC<LoadingProps> = ({
+    inverted,
+    content
+}) => {
+        return (
+        <Dimmer active inverted={inverted}>
+            <Loader content={content}/>
+        </Dimmer>);
+}
+
+export default Loading;
