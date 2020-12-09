f50aacd75798b33792c854b74d59551c729b124a
Tony <tony.zhou125@gmail.com>
2020-12-09 03:00:59 +0100

    Fixed HTTP Delete bug

--- Mon-Ami.Server/Mon-Ami.API/Controllers/ActivitiesController.cs
+++ Mon-Ami.Server/Mon-Ami.API/Controllers/ActivitiesController.cs
@@ -21,28 +21,28 @@ namespace Mon_Ami.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivityList(CancellationToken token)
+        public async Task<ActionResult<List<Activity>>> GetList(CancellationToken token)
         {
             List<Activity> result = await _mediator.Send(new List.Query(), token);
             return result;
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivity(Guid id, CancellationToken token)
+        public async Task<ActionResult<Activity>> Get(Guid id, CancellationToken token)
         {
             Activity result = await _mediator.Send(new Details.Query { Id = id }, token);
             return result;
         }
 
         [HttpPost]
-        public async Task<ActionResult<Unit>> CreateActivity(Create.Command command, CancellationToken token)
+        public async Task<ActionResult<Unit>> Create(Create.Command command, CancellationToken token)
         {
             Unit result = await _mediator.Send(command, token);
             return result;
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult<Unit>> EditActivity(Guid id, Edit.Command command, CancellationToken token)
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command, CancellationToken token)
         {
             command.Id = id;
             Unit result = await _mediator.Send(command, token);
@@ -50,10 +50,9 @@ namespace Mon_Ami.API.Controllers
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Unit>> DeleteActivity(Guid id, Delete.Command command, CancellationToken token)
+        public async Task<ActionResult<Unit>> Delete(Guid id)
         {
-            command.Id = id;
-            Unit result = await _mediator.Send(new Delete.Command { Id = id }, token);
+            Unit result = await _mediator.Send(new Delete.Command { Id = id });
             return result;
         }
     }
