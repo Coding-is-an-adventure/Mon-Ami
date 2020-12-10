cc27d29d44f566208671f3b4764dda3b269ffdad
Tony <tony.zhou125@gmail.com>
2020-12-10 02:18:31 +0100

    Update activityStore.ts
    Added activities observable
    Added activity loader action
    Added makeObservable in the constructor to resolve the store not updating.

--- mon-ami.client/src/app/stores/activityStore.ts
+++ mon-ami.client/src/app/stores/activityStore.ts
@@ -1,8 +1,27 @@
-import { observable } from "mobx";
+import { observable, action, makeObservable } from "mobx";
 import { createContext } from "react";
+import agent from "../api/agent";
+import { IActivity } from "../models/Activity";
 
 class ActivityStore {
-    @observable title ='Hello from mobx'
+  @observable activities: IActivity[] = [];
+  @observable initialLoading = false;
+
+  constructor() {
+    makeObservable(this)
+  }
+
+  @action loadActivities = () => {
+    this.initialLoading = true;
+    agent.Activities.list()
+      .then((activities) => {
+        activities.forEach((activity) => {
+          activity.date = activity.date.split(".")[0];
+          this.activities.push(activity);
+        });
+      })
+      .finally(() => (this.initialLoading = false));
+  };
 }
 
 export default createContext(new ActivityStore());
