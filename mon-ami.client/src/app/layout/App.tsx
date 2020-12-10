6111c13ce89849f6823a5cdee88105adcd2769f3
Tony <tony.zhou125@gmail.com>
2020-12-10 22:48:05 +0100

    Update App.tsx

--- mon-ami.client/src/app/layout/App.tsx
+++ mon-ami.client/src/app/layout/App.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useContext, Fragment } from "react";
 import { Container } from "semantic-ui-react";
 import NavigationBar from "../../features/navigation/NavigationBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import Loading from "../layout/Loading";
+import Loading from "./Loading";
 
 import ActivityStore from "../stores/activityStore";
 import { observer } from "mobx-react-lite";
