28060dc1f32f5efda5529bba92def7b2ee179ac8
Tony <tony.zhou125@gmail.com>
2020-12-11 11:18:53 +0100

    Create RestExeption.cs

---
+++ Mon-Ami.Server/API.Application/ErrorHandlers/RestExeption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+
+namespace API.Application.ErrorHandlers
+{
+    public class RestExeption : Exception
+    {
+        private readonly HttpStatusCode _code;
+        private readonly object _errors;
+
+        public RestExeption(HttpStatusCode code, object errors = null)
+        {
+            _code = code;
+            _errors = errors;
+        }
+    }
+}
