987725aa374309126d4b06f9adbfec5e966d3414
Tony <tony.zhou125@gmail.com>
2020-12-11 10:38:13 +0100

    Restyled the components
    -Restyled ugly components
    -Added static placeholder for the chat and followers.

---
+++ mon-ami.client/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Item, Button, Segment, Icon } from "semantic-ui-react";
+import { IActivity } from "../../../app/models/activity";
+
+interface ActivityListItemProps {
+  activity: IActivity;
+}
+
+const ActivityListItem: React.FC<ActivityListItemProps> = ({ activity }) => {
+  return (
+    <Segment.Group>
+      <Segment>
+        <Item.Group>
+          <Item>
+            <Item.Image size="tiny" circular src="/assets/user.png" />
+            <Item.Content>
+              <Item.Header as="a">{activity.title}</Item.Header>
+              <Item.Description>Hosted by Bernie</Item.Description>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+      </Segment>
+
+      <Segment>
+        <Icon name="clock" />
+        {activity.date}
+        <Icon name="marker" />
+        {activity.venue}, {activity.city}
+      </Segment>
+
+      <Segment secondary>Attendees: 0</Segment>
+
+      <Segment clearing>
+        <span>{activity.description}</span>
+        <Button
+          as={Link}
+          to={`/activities/${activity.id}`}
+          floated="right"
+          content="View"
+          color="blue"
+        />
+      </Segment>
+    </Segment.Group>
+  );
+};
+
+export default ActivityListItem;
--- mon-ami.client/src/features/activities/details/ActivityDetails.tsx
+++ mon-ami.client/src/features/activities/details/ActivityDetails.tsx
@@ -1,10 +1,13 @@
 import React, { useContext, useEffect } from "react";
 import { observer } from "mobx-react-lite";
-import { Card, Image, Button } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import ActivityStore from "../../../app/stores/activityStore";
 import { RouteComponentProps } from "react-router";
 import Loading from "../../../app/layout/Loading";
-import { Link } from "react-router-dom";
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from "./ActivityDetailedInfo";
+import ActivityDetailedChat from "./ActivityDetailedChat";
+import ActivityDetailedSideBar from "./ActivityDetailedSideBar";
 
 interface IDetailParameters {
   id: string;
@@ -15,11 +18,7 @@ const ActivityDetails: React.FC<RouteComponentProps<IDetailParameters>> = ({
   history,
 }) => {
   const activityStore = useContext(ActivityStore);
-  const {
-    activity,
-    loadActivity,
-    initialLoading,
-  } = activityStore;
+  const { activity, loadActivity, initialLoading } = activityStore;
 
   useEffect(() => {
     loadActivity(match.params.id);
@@ -30,39 +29,16 @@ const ActivityDetails: React.FC<RouteComponentProps<IDetailParameters>> = ({
   }
 
   return (
-    <Card fluid>
-      <Image
-        src={`/assets/categoryImages/${activity!.category}.jpg`}
-        wrapped
-        ui={false}
-      />
-      <Card.Content>
-        <Card.Header>{activity!.title}</Card.Header>
-        <Card.Meta>
-          <span>{activity!.date}</span>
-        </Card.Meta>
-        <Card.Description>
-          <span>{activity!.description}</span>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button.Group widths={2}>
-          <Button
-            as={Link}
-            to={`/manage/${activity.id}`}
-            basic
-            color="blue"
-            content="Edit"
-          />
-          <Button
-            onClick={() => history.push("/activities")}
-            basic
-            color="grey"
-            content="Cancel"
-          />
-        </Button.Group>
-      </Card.Content>
-    </Card>
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityDetailedHeader activity={activity} />
+        <ActivityDetailedInfo activity={activity}/>
+        <ActivityDetailedChat />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <ActivityDetailedSideBar />
+      </Grid.Column>
+    </Grid>
   );
 };
 
