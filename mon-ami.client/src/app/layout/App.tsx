5b8075792a7b634d92194fd5df2d8f80338ebaaf
Tony <tony.zhou125@gmail.com>
2020-12-11 12:20:26 +0100

    Update App.tsx
    Added NotFound Component to the routing.

--- mon-ami.client/src/app/layout/App.tsx
+++ mon-ami.client/src/app/layout/App.tsx
@@ -2,12 +2,18 @@ import React, { Fragment } from "react";
 
 import { Container } from "semantic-ui-react";
 
-import { Route, RouteComponentProps, withRouter } from "react-router-dom";
+import {
+  Route,
+  RouteComponentProps,
+  Switch,
+  withRouter,
+} from "react-router-dom";
 import NavigationBar from "../../features/navigation/NavigationBar";
 import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import ActivityForm from "../../features/activities/form/ActivityForm";
+import NotFound from "./NotFound";
 
 import { observer } from "mobx-react-lite";
 
@@ -21,13 +27,16 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
           <Fragment>
             <NavigationBar />
             <Container style={{ marginTop: "7em" }}>
-              <Route exact path="/activities" component={ActivityDashboard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={["/createActivity", "/manage/:id"]}
-                component={ActivityForm}
-              />
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={["/createActivity", "/manage/:id"]}
+                  component={ActivityForm}
+                />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </Fragment>
         )}
