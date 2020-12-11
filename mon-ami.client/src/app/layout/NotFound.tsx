8928061d88153df207d89342f299b37fa5dbced9
Tony <tony.zhou125@gmail.com>
2020-12-11 12:20:29 +0100

    Create NotFound.tsx

---
+++ mon-ami.client/src/app/layout/NotFound.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Segment, Header, Button, Icon } from 'semantic-ui-react';
+
+const NotFound: React.FC = () => {
+        return (
+            <Segment placeholder>
+            <Header icon>
+                <Icon name='search' />
+                Oops - We've have looked everywhere but we couldn't find what you were looking for.
+            </Header>
+            <Segment.Inline>
+                <Button as={Link} to='/activities' primary>
+                    Return to Activities page
+                </Button>
+            </Segment.Inline>
+        </Segment>
+        );
+}
+
+export default NotFound;
