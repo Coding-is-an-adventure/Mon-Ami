4414ae0364e6a44193debdd7c8d3950fde22430d
Tony <tony.zhou125@gmail.com>
2020-12-11 11:05:17 +0100

    Update Create.cs
    Added basic validation using FluentValidation.

--- Mon-Ami.Server/API.Application/Activities/Create.cs
+++ Mon-Ami.Server/API.Application/Activities/Create.cs
@@ -35,6 +35,11 @@ namespace API.Application.Activities
             public CommandValidator()
             {
                 RuleFor(x => x.Title).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
+                RuleFor(x => x.Category).NotEmpty();
+                RuleFor(x => x.Date).NotEmpty();
+                RuleFor(x => x.City).NotEmpty();
+                RuleFor(x => x.Venue).NotEmpty();
             }
         }
 
