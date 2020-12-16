cbbb190c3fa18fd954275a0e7b956b8ee987adb3
Tony <tony.zhou125@gmail.com>
2020-12-17 00:59:04 +0100

    Update DateInput.tsx
    Fixed a bug which coused the component to be unable to throw a validation error when neccesary.

--- mon-ami.client/src/app/common/form/DateInput.tsx
+++ mon-ami.client/src/app/common/form/DateInput.tsx
@@ -6,34 +6,35 @@ import { DateTimePicker } from "react-widgets";
 interface IProps extends FieldRenderProps<Date, HTMLElement>, FormFieldProps {}
 
 const DateInput: React.FC<IProps> = ({
-    input,
-    width,
-    placeholder,
-    date = false,
-    time = false,
-    meta: touched,
-    error,
-    id,
-    ...props
-  }) => {
-    return (
-      <Form.Field error={touched && !!error} width={width}>
-        <DateTimePicker
-          id={String(id)}
-          placeholder={placeholder}
-          value={input.value || null}
-          onChange={input.onChange}
-          date={date}
-          time={time}
-          {...props}
-        />
-        {touched && error && (
-          <Label basic color="red">
-            {error}
-          </Label>
-        )}
-      </Form.Field>
-    );
-  };
+  id,
+  input,
+  width,
+  placeholder,
+  date = false,
+  time = false,
+  meta: { touched, error },
+  ...rest
+}) => {
+  return (
+    <Form.Field error={touched && !!error} width={width}>
+      <DateTimePicker
+        id={String(id)}
+        placeholder={placeholder}
+        value={input.value || null}
+        onChange={input.onChange}
+        onBlur={input.onBlur}
+        onKeyDown={(e) => e.preventDefault()}
+        date={date}
+        time={time}
+        {...rest}
+      />
+      {touched && error && (
+        <Label basic color="red">
+          {error}
+        </Label>
+      )}
+    </Form.Field>
+  );
+};
 
-  export default DateInput;
+export default DateInput;
