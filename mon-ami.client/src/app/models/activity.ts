cb3ad6230996022bd9377a6d1f68b59c5de96e58
Tony <tony.zhou125@gmail.com>
2020-12-17 01:00:08 +0100

    Update activity.ts
    Added a class which represents the activity.

--- mon-ami.client/src/app/models/activity.ts
+++ mon-ami.client/src/app/models/activity.ts
@@ -11,3 +11,21 @@ export interface IActivity {
 export interface IActivityFormValues extends Partial<IActivity> {
    time?: Date
 }
+
+export class ActivityFormModel implements IActivityFormValues {
+  id?: string = undefined;
+  title: string = "";
+  category: string = "";
+  description: string = "";
+  date?: Date = undefined;
+  time?: Date = undefined;
+  city: string = "";
+  venue: string = "";
+
+  constructor(initialise?: IActivityFormValues) {
+    if (initialise && initialise.date) {
+      initialise.time = initialise.date
+    }
+    Object.assign(this, initialise);
+  }
+}
