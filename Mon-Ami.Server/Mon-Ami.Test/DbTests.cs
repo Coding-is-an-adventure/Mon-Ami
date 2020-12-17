5cf5de1b34ffd68f6e651edfb78a9ae3a9c7d88a
Tony <tony.zhou125@gmail.com>
2020-12-17 01:01:43 +0100

    Added unit testing for the BE

---
+++ Mon-Ami.Server/Mon-Ami.Test/DbTests.cs
@@ -0,0 +1,32 @@
+﻿//using API.Application.Activities;
+using API.Domain;
+using API.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Mon_Ami.Test
+{
+    public class DBTests
+    {
+        //[Fact]
+        //public void Create_Valid_ReturnsTrue()
+        //{
+        //    // Arrange
+        //    var model = new Activity();
+
+        //    var options = new DbContextOptionsBuilder<DataContext>()
+        //        .UseInMemoryDatabase(databaseName: "testDB")
+        //        .EnableSensitiveDataLogging()
+        //        .EnableDetailedErrors()
+        //        .Options;
+
+        //    var context = new DataContext(options);
+
+        //    // Act 
+        //    var result = context.Activities.Add(model);
+
+        //    // Assert
+        //    Assert.True(false);
+        //}
+    }
+}
