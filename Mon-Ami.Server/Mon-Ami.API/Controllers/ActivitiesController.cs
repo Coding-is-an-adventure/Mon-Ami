e8b1239103cc4fb69263123b6db2fbd369fde84f
Tony <tony.zhou125@gmail.com>
2021-01-04 23:35:11 +0100

    Update ActivitiesController.cs
    Moved mediator logic into another class.

--- Mon-Ami.Server/Mon-Ami.API/Controllers/ActivitiesController.cs
+++ Mon-Ami.Server/Mon-Ami.API/Controllers/ActivitiesController.cs
@@ -9,35 +9,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Mon_Ami.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ActivitiesController : ControllerBase
+    public class ActivitiesController : APIControllerBase
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetList(CancellationToken token)
         {
-            List<Activity> result = await _mediator.Send(new List.Query(), token);
+            List<Activity> result = await Mediator.Send(new List.Query(), token);
             return result;
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> Get(Guid id, CancellationToken token)
         {
-            Activity result = await _mediator.Send(new Details.Query { Id = id }, token);
+            Activity result = await Mediator.Send(new Details.Query { Id = id }, token);
             return result;
         }
 
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(Create.Command command, CancellationToken token)
         {
-            Unit result = await _mediator.Send(command, token);
+            Unit result = await Mediator.Send(command, token);
             return result;
         }
 
@@ -45,14 +36,14 @@ namespace Mon_Ami.API.Controllers
         public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command, CancellationToken token)
         {
             command.Id = id;
-            Unit result = await _mediator.Send(command, token);
+            Unit result = await Mediator.Send(command, token);
             return result;
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Delete(Guid id, CancellationToken token)
         {
-            Unit result = await _mediator.Send(new Delete.Command { Id = id }, token);
+            Unit result = await Mediator.Send(new Delete.Command { Id = id }, token);
             return result;
         }
     }
