78be819fca751026ac985cce4b28a79015534ed9
Tony <tony.zhou125@gmail.com>
2021-01-04 23:35:15 +0100

    Create UserController.cs

---
+++ Mon-Ami.Server/Mon-Ami.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Application.User;
+using API.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mon_Ami.API.Controllers
+{
+    public class UserController : APIControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(Login.Query query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
