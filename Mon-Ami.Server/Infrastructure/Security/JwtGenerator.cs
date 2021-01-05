85a6a1e5b677e78ad01e929e7e9a39ecf972684d
Tony <tony.zhou125@gmail.com>
2021-01-05 02:15:12 +0100

    Update JwtGenerator.cs
    Added the implementation for generation a JwtToken.

--- Mon-Ami.Server/Infrastructure/Security/JwtGenerator.cs
+++ Mon-Ami.Server/Infrastructure/Security/JwtGenerator.cs
@@ -1,18 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using API.Application.Interfaces;
 using API.Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Infrastructure.Security
 {
     public class JwtGenerator : IJwtGenerator
     {
+        private readonly SymmetricSecurityKey _key;
+
+        public JwtGenerator(IConfiguration configuration)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
+        }
+
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(GenerateClaims(user)),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = GenerateCredentials(),
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        private List<Claim> GenerateClaims(AppUser user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+            return claims;
+        }
+
+        private SigningCredentials GenerateCredentials()
+        {
+            SigningCredentials credentials = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            return credentials;
         }
     }
 }
