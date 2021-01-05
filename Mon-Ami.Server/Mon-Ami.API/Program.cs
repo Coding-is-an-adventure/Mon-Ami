9abb3412d8031b41244058bd99318b5b1b4c5225
Tony <tony.zhou125@gmail.com>
2021-01-05 12:19:58 +0100

    Update Program.cs
    This doesn't work in Docker.

--- Mon-Ami.Server/Mon-Ami.API/Program.cs
+++ Mon-Ami.Server/Mon-Ami.API/Program.cs
@@ -19,32 +19,32 @@ namespace Mon_Ami.API
         [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1005:Single line comments should begin with single space", Justification = "These comments are only used for commenting out code.")]
         public static void Main(string[] args)
         {
-            //CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
 
             // Inputs test values into the database. Don't use this in production!!
-            IHost host = CreateHostBuilder(args).Build();
-            using (IServiceScope scope = host.Services.CreateScope())
-            {
-                IServiceProvider services = scope.ServiceProvider;
+            // IHost host = CreateHostBuilder(args).Build();
+            //using (IServiceScope scope = host.Services.CreateScope())
+            //{
+            //    IServiceProvider services = scope.ServiceProvider;
 
-                try
-                {
-                    DataContext context = services.GetRequiredService<DataContext>();
-                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
+            //    try
+            //    {
+            //        DataContext context = services.GetRequiredService<DataContext>();
+            //        var userManager = services.GetRequiredService<UserManager<AppUser>>();
 
-                    // Auto applies the latest database migrations
-                    context.Database.Migrate();
+            //        // Auto applies the latest database migrations
+            //        context.Database.Migrate();
 
-                    // Auto seeds the database with sample data.
-                    Seed.SeedDate(context, userManager).Wait();
-                }
-                catch (Exception exception)
-                {
-                    ILogger logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(exception, "An error occured during the database migration");
-                }
-            }
-            host.Run();
+            //        // Auto seeds the database with sample data.
+            //        Seed.SeedDate(context, userManager).Wait();
+            //    }
+            //    catch (Exception exception)
+            //    {
+            //        ILogger logger = services.GetRequiredService<ILogger<Program>>();
+            //        logger.LogError(exception, "An error occured during the database migration");
+            //    }
+            //}
+            //host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
