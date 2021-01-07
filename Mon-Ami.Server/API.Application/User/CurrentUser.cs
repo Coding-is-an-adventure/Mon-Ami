4aff07c6bc41a6a9475771b6ee235bd89f7123e2
Tony <tony.zhou125@gmail.com>
2021-01-07 03:40:01 +0100

    Create CurrentUser.cs

---
+++ Mon-Ami.Server/API.Application/User/CurrentUser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using API.Application.ErrorHandlers;
+using API.Application.Interfaces;
+using API.Domain;
+using API.Persistence;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Application.User
+{
+    public class CurrentUser
+    {
+        public class Query : IRequest<User>
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, User>
+        {
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(UserManager<AppUser> userManager, IJwtGenerator jwtGenerator, IUserAccessor userAccessor)
+            {
+                _userManager = userManager;
+                _jwtGenerator = jwtGenerator;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<User> Handle(
+                Query request,
+                CancellationToken token)
+            {
+                AppUser user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUsername());
+
+                if (user == null)
+                {
+                    throw new RestException(HttpStatusCode.NotFound, new { activity = "Not found" });
+                }
+
+                return new User
+                {
+                    DisplayName = user.DisplayName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    UserName = user.UserName,
+                    Image = null
+                };
+            }
+        }
+    }
+}
