97fd0fe86511e1498039f0ee034c5b73c2a1599c
Tony <tony.zhou125@gmail.com>
2021-01-10 23:14:40 +0100

    Update NotFound.tsx

--- mon-ami.client/src/app/layout/NotFound.tsx
+++ mon-ami.client/src/app/layout/NotFound.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { Segment, Header, Button, Icon } from 'semantic-ui-react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Segment, Header, Button, Icon } from "semantic-ui-react";
 
 const NotFound: React.FC = () => {
-        return (
-            <Segment placeholder>
-            <Header icon>
-                <Icon name='search' />
-                Oops - We've have looked everywhere but we couldn't find what you were looking for.
-            </Header>
-            <Segment.Inline>
-                <Button as={Link} to='/activities' primary>
-                    Return to Activities page
-                </Button>
-            </Segment.Inline>
-        </Segment>
-        );
-}
+  return (
+    <Segment placeholder>
+      <Header icon>
+        <Icon name="search" />
+        Oops - We've have looked everywhere but we couldn't find what you were
+        looking for.
+      </Header>
+      <Segment.Inline>
+        <Button as={Link} to="/activities" primary>
+          Return to Activities page
+        </Button>
+      </Segment.Inline>
+    </Segment>
+  );
+};
 
 export default NotFound;
