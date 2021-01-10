c8aef152fe0e79275ff97164c55e698cb6757b34
Tony <tony.zhou125@gmail.com>
2021-01-11 00:59:31 +0100

    Update HomePage.tsx
    Added different options depending on if the user is logged in or not.

--- mon-ami.client/src/features/home/HomePage.tsx
+++ mon-ami.client/src/features/home/HomePage.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { Fragment, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Container, Segment, Header, Image, Button } from "semantic-ui-react";
+import { RootStoreContext } from "../../app/stores/rootStore";
 
 const HomePage: React.FC = () => {
+  const rootStore = useContext(RootStoreContext);
+  const { isLoggedIn, user } = rootStore.userStore;
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container text>
@@ -15,10 +18,28 @@ const HomePage: React.FC = () => {
           />
           Mon Ami
         </Header>
-        <Header as="h2" inverted content="Welcome to Mon Ami" />
-        <Button as={Link} to="/login" size="huge" inverted>
-          Login
-        </Button>
+        {isLoggedIn && user ? (
+          <Fragment>
+            <Header
+              as="h2"
+              inverted
+              content={`Welcome back ${user.displayName}!`}
+            />
+            <Button as={Link} to="/activities" size="huge" inverted>
+              Go to the activities!
+            </Button>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Header as="h2" inverted content="Welcome to Mon Ami" />
+            <Button as={Link} to="/login" size="huge" inverted>
+              Login
+            </Button>
+            <Button as={Link} to="/register" size="huge" inverted>
+              Register
+            </Button>
+          </Fragment>
+        )}
       </Container>
     </Segment>
   );
