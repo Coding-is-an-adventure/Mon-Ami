476b2f53f71702b367c4711bf4fd11c6bb7c9391
Tony <tony.zhou125@gmail.com>
2021-01-19 22:49:57 +0100

    Create PictureAccessor.cs

---
+++ Mon-Ami.Server/Infrastructure/Pictures/PictureAccessor.cs
@@ -0,0 +1,24 @@
+﻿using API.Application.Interfaces;
+using API.Application.Pictures;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Pictures
+{
+    public class PictureAccessor : IPictureAccessor
+    {
+        public PictureAccessor()
+        {
+            // d
+        }
+
+        public PictureUploadResult AddPicture(IFormFile file)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string DeletePicture(string publicId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
