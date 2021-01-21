3ca39ee35705f4fc651a9b10d46b624b05cc35f1
Tony <tony.zhou125@gmail.com>
2021-01-21 22:09:31 +0100

    Create ChatHub.cs

---
+++ Mon-Ami.Server/Mon-Ami.API/SignalR/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using API.Application.Comments;
+using API.Application.Comments.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Mon_Ami.API.SignalR
+{
+    public class ChatHub : Hub
+    {
+        private readonly IMediator _mediator;
+
+        public ChatHub(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task SendComment(Create.Command command)
+        {
+            string username = Context
+                             .User?
+                             .Claims?
+                             .FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            command.Username = username;
+
+            CommentDTO comment = await _mediator.Send(command);
+
+            await Clients.All.SendAsync("ReceiveComment", comment);
+        }
+    }
+}
