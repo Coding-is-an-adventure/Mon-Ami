6d8cb289daa3a5ef643367713ecb0617d0ef0207
Tony <tony.zhou125@gmail.com>
2021-01-21 04:57:01 +0100

    Update ActivityDetailedHeader.tsx
    Added link to the profile of the host of the activity.

--- mon-ami.client/src/features/activities/details/ActivityDetailedHeader.tsx
+++ mon-ami.client/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -26,6 +26,7 @@ const activityImageTextStyle = {
 const ActivityDetailedHeader: React.FC<IActivityDetailedProps> = ({
   activity,
 }) => {
+  const host = activity.attendees.filter((x) => x.isHost)[0];
   const rootStore = useContext(RootStoreContext);
   const { attendActivity, cancelAttendance, loading } = rootStore.activityStore;
   return (
@@ -47,7 +48,7 @@ const ActivityDetailedHeader: React.FC<IActivityDetailedProps> = ({
                 />
                 <p>{format(activity.date, "eeee do MMMM")}</p>
                 <p>
-                  Hosted by <strong>Bob</strong>
+                  Hosted by <Link to={`/profile/${host.username}`}><strong>{host.displayName}</strong></Link>
                 </p>
               </Item.Content>
             </Item>
