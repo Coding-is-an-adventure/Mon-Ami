1f68c09e41caaefa4d466bbaec0aca96f65a63f2
Tony <tony.zhou125@gmail.com>
2021-01-21 21:46:14 +0100

    Create MappingProfile.cs

---
+++ Mon-Ami.Server/API.Application/Comments/DTOs/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using API.Domain;
+using AutoMapper;
+
+namespace API.Application.Comments.DTOs
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Comment, CommentDTO>()
+                .ForMember(
+                    destination => destination.Username,
+                    option => option.MapFrom(source => source.Author.UserName))
+                .ForMember(
+                    destination => destination.DisplayName,
+                    option => option.MapFrom(source => source.Author.DisplayName))
+                .ForMember(
+                    destination => destination.Image,
+                    option => option.MapFrom(source => source.Author.Pictures.FirstOrDefault(x => x.IsMain).Url));
+        }
+    }
+}
